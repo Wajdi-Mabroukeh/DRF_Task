004a974b52dd808bf7f34ff74518994a72278943
Wajeed-Mabroukeh <wajeed.mabroukeh@gmail.com>
2021-03-24 09:01:12 +0200

    add serializer

---
+++ task/account/serializers.py
@@ -0,0 +1,10 @@
+# from rest_framework import serializers
+# from .models import Account
+#
+#
+# class RegistrationSerializer(serializers.ModelSerializer):
+#     password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+#
+#     class Meta:
+#         model = Account
+#         fields = ['email']
