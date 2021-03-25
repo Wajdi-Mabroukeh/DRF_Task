1227b7287af4d94eff860538d45ea47202fd3edc
Wajdi-Mabroukeh <wajdi1077@gmail.com>
2021-03-25 08:45:35 +0200

    register and login APIs

--- task/account/serializers.py
+++ task/account/serializers.py
@@ -8,49 +8,39 @@ from django.contrib.auth.password_validation import validate_password
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
-        fields = ['county', 'name', 'city']
+        fields = '__all__'
 
 
-class RegisterSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(
-        required=True,
-        validators=[UniqueValidator(queryset=User.objects.all())]
-    )
 
-    addresses = AddressSerializer(many=True)
+class AccountSerializer(serializers.ModelSerializer):
 
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = Account
-        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name', 'account_type', 'addresses')
-        extra_kwargs = {
-            'username': {'required': True}
-        }
+        fields = '__all__'
 
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
-
-        return attrs
-
-    def create(self, validated_data):
-        addresses = validated_data.pop('addresses')
-        account_type = validated_data.pop('account_type')
-        user = User.objects.create(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name']
-        )
-
-        user.set_password(validated_data['password'])
-        user.save()
-
-        user.account.type = account_type;
-        for address in addresses:
-            Address.objects.create(account=user.account, **address)
-
-        user.save()
-        return user
+    # def validate(self, attrs):
+    #     if attrs['password'] != attrs['password2']:
+    #         raise serializers.ValidationError({"password": "Password fields didn't match."})
+    #
+    #     return attrs
+    #
+    # def create(self, validated_data):
+    #     addresses = validated_data.pop('addresses')
+    #     account_type = validated_data.pop('account_type')
+    #     user = User.objects.create(
+    #         username=validated_data['username'],
+    #         email=validated_data['email'],
+    #         first_name=validated_data['first_name'],
+    #         last_name=validated_data['last_name']
+    #     )
+    #
+    #     user.set_password(validated_data['password'])
+    #     user.save()
+    #
+    #     user.account.type = account_type;
+    #     for address in addresses:
+    #         Address.objects.create(account=user.account, **address)
+    #
+    #     user.save()
+    #     return user
--- task/account/views.py
+++ task/account/views.py
@@ -1,11 +1,92 @@
-from .models import Account
+import json
+
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+
+from .models import Account, Address
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
+from django.core.validators import validate_email
+from django.contrib.auth import authenticate, login
+from rest_framework.authtoken.models import Token
 
-from .serializers import RegisterSerializer
+from .serializers import AccountSerializer
+
+
+def is_validate(**kwargs):
+    if kwargs['password'] != kwargs['password2']:
+        raise ValidationError({"password": "Password fields didn't match."})
+
+    try:
+        validate_email(kwargs['email'])
+    except ValidationError:
+        raise ValidationError({"email": "email format not correct"})
+
+    if User.objects.filter(username=kwargs['username']).exists():
+        raise ValidationError({"username": "username exists before. try another one"})
+
+    if User.objects.filter(email=kwargs['email']).exists():
+        raise ValidationError({"email": "email exists before. try another one"})
+
+    return True
 
 
 class RegisterView(CreateAPIView):
-    queryset = Account.objects.all()
-    permission_classes = (IsAuthenticated,)
-    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        first_name = request.data.get('first_name')
+        last_name = request.data.get('last_name')
+        password = request.data.get('password')
+        password2 = request.data.get('password2')
+        email = request.data.get('email')
+        addresses = request.data.get('addresses')
+        account_type = request.data.get('account_type')
+
+        # validation
+        is_validate(password=password, password2=password2,
+                    email=email, username=username)
+
+        # create new user
+        user = User.objects.create(
+            username=username,
+            email=email,
+            first_name=first_name,
+            last_name=last_name
+        )
+
+        user.set_password(password)
+        user.save()
+
+        user.account.account_type = account_type
+        if addresses:
+            for address_temp in addresses:
+                Address.objects.get_or_create(account=user.account,
+                                              city=address_temp.get('city'),
+                                              name=address_temp.get('name'),
+                                              county=address_temp.get('country'))
+
+        user.save()
+        data = AccountSerializer(user.account).data
+
+        return Response(data=data, status=status.HTTP_201_CREATED)
+
+
+class LoginView(CreateAPIView):
+
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            token, _ = Token.objects.get_or_create(user=user)
+            data = {"username": user.username, "token": str(token)}
+            return JsonResponse(data)
+        else:
+            raise ValidationError({"login": "invalid login"})
